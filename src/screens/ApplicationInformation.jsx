95867fd1236cdf08ca81be5a05984c53f973cdf9
neon4on <vkam37@gmail.com>
2024-05-17 18:14:52 +0400

    Done Task 1 and Add Image

--- src/screens/ApplicationInformation.jsx
+++ src/screens/ApplicationInformation.jsx
@@ -1,13 +1,16 @@
+// ApplicationInformation.js
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image, ScrollView } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#fff',
+  },
+  contentContainer: {
+    alignItems: 'center',
+    paddingVertical: 20,
   },
   title: {
     fontSize: 24,
@@ -27,6 +30,11 @@ const styles = StyleSheet.create({
     color: 'green',
     textAlign: 'center',
   },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
 });
 
 const ApplicationInformation = () => {
@@ -34,17 +42,20 @@ const ApplicationInformation = () => {
   const appVersion = DeviceInfo.getVersion();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Application Information</Text>
-      <View style={styles.infoContainer}>
-        <Text style={styles.infoLabel}>App Name:</Text>
-        <Text style={styles.infoValue}>{appName}</Text>
+    <ScrollView style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>Application Information</Text>
+        <Image source={require('../assets/kit.jpeg')} style={styles.image} />
+        <View style={styles.infoContainer}>
+          <Text style={styles.infoLabel}>App Name:</Text>
+          <Text style={styles.infoValue}>{appName}</Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.infoLabel}>App Version:</Text>
+          <Text style={styles.infoValue}>{appVersion}</Text>
+        </View>
       </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.infoLabel}>App Version:</Text>
-        <Text style={styles.infoValue}>{appVersion}</Text>
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
